1ca88af016a236cf8caeb97e4524465bd53cd5e9
LukasMut <muttenthaler@cbs.mpg.de>
2022-06-28 13:05:15 +0200

    added things-aligned and things

--- data/__init__.py
+++ data/__init__.py
@@ -2,25 +2,34 @@ from .things import THINGSBehavior
 from .cifar import CIFAR100Triplet, CIFAR100CoarseTriplet, CIFAR10Triplet
 from .things import THINGSTriplet, THINGSBehavior
 
-DATASETS = ['cifar100-coarse', 'cifar100-fine', 'cifar10', 'things']
+DATASETS = ['cifar100-coarse', 'cifar100-fine', 'cifar10', 'things', 'things-aligned']
 
 
 def load_dataset(name, data_dir, transform):
     if name == 'cifar100-coarse':
-        dataset = CIFAR100CoarseTriplet(root=data_dir, train=True,
-                                        download=True, transform=transform,
-                                        samples=10000, seed=0)
+        dataset = CIFAR100CoarseTriplet(
+            root=data_dir, train=True,
+            download=True, transform=transform,
+            samples=10000, seed=0)
     elif name == 'cifar100-fine':
-        dataset = CIFAR100Triplet(root=data_dir, train=True,
-                                  download=True, transform=transform,
-                                  samples=10000, seed=0)
+        dataset = CIFAR100Triplet(
+            root=data_dir, train=True,
+            download=True, transform=transform,
+            samples=10000, seed=0)
     elif name == 'cifar10':
-        dataset = CIFAR10Triplet(root=data_dir, train=True,
-                                 download=True, transform=transform,
-                                 samples=10000, seed=0)
+        dataset = CIFAR10Triplet(
+            root=data_dir, train=True,
+            download=True, transform=transform,
+            samples=10000, seed=0)
     elif name == 'things':
-        dataset = THINGSBehavior(root=data_dir, download=True, transform=transform)
+        dataset = THINGSBehavior(
+            root=data_dir, aligned=False,
+            download=True, transform=transform)
+    elif name == 'things-aligned':
+        dataset = THINGSBehavior(
+            root=data_dir, aligned=True,
+            download=True, transform=transform)
     else:
-        raise ValueError('Unknown dataset')
+        raise ValueError('\nUnknown dataset\n')
 
     return dataset
