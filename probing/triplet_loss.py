3793976830445e9efeeebf3e48fe8a3dfde26108
LukasMut <muttenthaler@cbs.mpg.de>
2022-08-10 18:52:02 +0200

    added triplet loss class

--- probing/triplet_loss.py
+++ probing/triplet_loss.py
@@ -7,7 +7,7 @@ Tensor = torch.Tensor
 
 
 class TripletLoss(nn.Module):
-    def __init__(self, temperature: float) -> None:
+    def __init__(self, temperature: float = 1.) -> None:
         super().__init__()
         self.temperature = temperature
 
@@ -15,7 +15,7 @@ class TripletLoss(nn.Module):
         return torch.log(torch.sum(torch.exp(torch.stack(dots)), dim=0))
 
     def log_softmax(self, dots: Tuple[Tensor]) -> Tensor:
-        return dots[0] - self.logsumexp(dots)
+        return dots[0] / self.temperature - self.logsumexp(dots / self.temperature)
 
     def cross_entropy_loss(self, dots: Tuple[Tensor]) -> Tensor:
         return torch.mean(-self.log_softmax(dots))
