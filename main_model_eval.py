605bdd5cf561c7f39d1be01229f0777047eb9129
LukasMut <muttenthaler@cbs.mpg.de>
2022-08-11 10:32:20 +0200

    fixed typo

--- main_model_eval.py
+++ main_model_eval.py
@@ -10,7 +10,7 @@ import numpy as np
 import pandas as pd
 import torch
 from ml_collections import config_dict
-from torch.evaluation.data import DataLoader
+from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 import evaluation
