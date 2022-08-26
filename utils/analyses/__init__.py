732bc7b8370d9d395cda3168135f6dc135828c6a
LukasMut <muttenthaler@cbs.mpg.de>
2022-08-26 14:26:51 +0200

    small fix in init file

--- utils/analyses/__init__.py
+++ utils/analyses/__init__.py
@@ -3,4 +3,4 @@ from .failures import Failures
 from .families import Families
 from .helpers import get_family_name, merge_results
 from .partition import Partition
-from .trainig_mapping import Mapper
+from .training_mapping import Mapper
