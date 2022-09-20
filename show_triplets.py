329727d7fe4769ce473d32109cfe753da2218a55
Jonas Dippel <jonas.dippel@gmx.de>
2022-09-20 13:49:26 +0200

    adapted show triplets script

--- show_triplets.py
+++ show_triplets.py
@@ -5,19 +5,31 @@ import torch
 import argparse
 from data import load_dataset, DATASETS
 
+color = 'lime'
+plt.rcParams['axes.edgecolor'] = color
+
 parser = argparse.ArgumentParser()
-parser.add_argument('--data-dir')
-parser.add_argument('--dataset', default='cifar100', choices=DATASETS)
+parser.add_argument('--data-dir', default='resources/datasets')
+parser.add_argument('--dataset', default='cifar100-coarse', choices=DATASETS)
 args = parser.parse_args()
 
-dataset = load_dataset(name=args.dataset, data_dir=args.data_dir, transform=transforms.ToTensor())
+dataset = load_dataset(name=args.dataset, data_dir=args.data_dir)
 
-dl = DataLoader(dataset, batch_size=1)
-it = iter(dl)
-for _ in range(10):
-    x1, x2, x3, y = next(it)
+triplets = dataset.get_triplets()
+for k in range(20):
+    i1, i2, i3 = triplets[k]
     fig, ax = plt.subplots(1, 3)
-    for i, x in enumerate([x1, x2, x3]):
-        ax[i].imshow(torch.permute(x[0], (1, 2, 0)))
-        ax[i].axis('off')
-    plt.show()
+    for i, idx in enumerate([i1, i2, i3]):
+        img, target = dataset[idx]
+        img = img.resize((224, 224))
+        ax[i].imshow(img)
+        if i == 2:
+            ax[i].patch.set_edgecolor(color)
+            ax[i].patch.set_linewidth('5')
+            ax[i].set_xticks([])
+            ax[i].set_yticks([])
+        else:
+            ax[i].axis('off')
+
+    plt.tight_layout()
+    plt.savefig(f'resources/plots/cifar_images/{k}.png')
