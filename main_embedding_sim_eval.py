ad7522373eaacf1c071707c47183cec0c37cf3cf
LukasMut <muttenthaler@cbs.mpg.de>
2022-11-17 14:48:22 +0100

    added embedding evaluation for pairwise similarities

--- main_embedding_sim_eval.py
+++ main_embedding_sim_eval.py
@@ -8,6 +8,7 @@ import numpy as np
 import pandas as pd
 import torch
 from ml_collections import config_dict
+from thingsvision.core.rsa import compute_rdm, correlate_rdms
 from tqdm import tqdm
 
 import utils
@@ -30,19 +31,21 @@ def parseargs():
     aa("--module", type=str, default="penultimate",
         choices=["logits", "penultimate"],
         help="module for which to extract features")
-    aa("--model_dict_path", type=str, default="/home/space/datasets/things/model_dict.json",
+    aa("--model_dict_path", type=str,
+        default="/home/space/datasets/things/model_dict.json",
         help="Path to the model_dict.json")
     aa("--distance", type=str, default="cosine",
         choices=["cosine", "euclidean"],
         help="distance function used for predicting the odd-one-out")
-    aa("--out_path", type=str, help="path/to/results", default='/home/space/datasets/things/results')
+    aa("--out_path", type=str, help="path/to/results",
+        default="/home/space/datasets/things/results")
     aa("--num_threads", type=int, default=4,
         help="number of threads used for intraop parallelism on CPU; use only if device is CPU")
     aa("--rnd_seed", type=int, default=42,
         help="random seed for reproducibility of results")
     aa("--verbose", action="store_true",
         help="show print statements about model performance during training")
-    aa("--cifar100", action='store_true')
+    aa("--cifar100", action="store_true")
     args = parser.parse_args()
     return args
 
@@ -78,9 +81,10 @@ def create_hyperparam_dicts(args, model_names) -> Tuple[FrozenDict, FrozenDict]:
         warnings.warn(
             f"\nCould not find model config dict in {args.model_dict_path}.\nSetting temperature values to 1.0.\n"
         )
-    model_cfg.source = args.embeddings_root.split("/")[-1] 
+    model_cfg.source = args.embeddings_root.split("/")[-1]
     model_cfg = config_dict.FrozenConfigDict(model_cfg)
     data_cfg.root = args.data_root
+    data_cfg.name = args.dataset
     data_cfg = config_dict.FrozenConfigDict(data_cfg)
     return model_cfg, data_cfg
 
@@ -95,7 +99,7 @@ def evaluate(args) -> None:
         embeddings_root=args.embeddings_root,
         object_names=object_names,
         module="embeddings" if args.module == "penultimate" else "logits",
-        thing_sort=not args.cifar100
+        thing_sort=not args.cifar100,
     )
     model_cfg, data_cfg = create_hyperparam_dicts(args, embeddings.keys())
     dataset = load_dataset(
@@ -105,42 +109,28 @@ def evaluate(args) -> None:
     results = []
 
     model_features = dict()
-    for i, (model_name, features) in tqdm(enumerate(embeddings.items()), desc="Model"):
-        family = utils.analyses.get_family_name(model_name) 
-        triplets = dataset.get_triplets()
-        choices, probas = utils.evaluation.get_predictions(
-            features=features,
-            triplets=triplets,
-            temperature=model_cfg.temperatures[i],
-            dist=args.distance,
-        )
-
-        acc = utils.evaluation.accuracy(choices)
-        entropies = utils.evaluation.ventropy(probas)
-        mean_entropy = entropies.mean().item()
+    for model_name, features in tqdm(embeddings.items(), desc="Model"):
+        family_name = utils.analyses.get_family_name(model_name)
+        rdm_dnn = compute_rdm(features, method="correlation")
+        rdm_humans = dataset.get_rdm()
+        corr_coef = correlate_rdms(rdm_dnn, rdm_humans, correlation="spearman")
         if args.verbose:
             print(
-                f"\nModel: {model_name}, Family: {family}, Zero-shot accuracy: {acc:.4f}, Average triplet entropy: {mean_entropy:.3f}\n"
+                f"\nModel: {model_name}, Family: {family_name}, Correlation coefficient: {corr_coef:.4f}\n"
             )
         summary = {
-            "model" : model_name,
-            "zero-shot" : acc,
-            "choices" : choices.cpu().numpy(),
-            "entropies" : entropies.cpu().numpy(),
-            "probas" : probas.cpu().numpy(),
-            "source" : model_cfg.source,
-            "family" : family,
+            "model": model_name,
+            "zero-shot": corr_coef,
+            "source": model_cfg.source,
+            "family": family_name,
+            "dataset": data_cfg.name,
         }
         results.append(summary)
         model_features[model_name] = features
 
     # convert results into Pandas DataFrame
     results = pd.DataFrame(results)
-    failures = utils.evaluation.get_failures(results)
-
-    out_path = os.path.join(
-        args.out_path, args.dataset, model_cfg.source, args.module
-    )
+    out_path = os.path.join(args.out_path, args.dataset, model_cfg.source, args.module)
     if not os.path.exists(out_path):
         print("\nCreating output directory...\n")
         os.makedirs(out_path)
@@ -148,7 +138,6 @@ def evaluate(args) -> None:
     # save dataframe to pickle to preserve data types after loading
     # load back with pd.read_pickle(/path/to/file/pkl)
     results.to_pickle(os.path.join(out_path, "results.pkl"))
-    failures.to_pickle(os.path.join(out_path, "failures.pkl"))
     utils.evaluation.save_features(features=model_features, out_path=out_path)
 
 
