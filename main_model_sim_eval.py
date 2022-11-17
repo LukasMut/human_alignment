d09c0a150767880af1b392e68e767125185e7b89
LukasMut <muttenthaler@cbs.mpg.de>
2022-11-17 15:46:37 +0100

    updated sim eval

--- main_model_sim_eval.py
+++ main_model_sim_eval.py
@@ -36,7 +36,8 @@ def parseargs():
         help="models for which we want to extract featues")
     aa("--module", type=str, choices=["logits", "penultimate"],
         help="module for which to extract features")
-    aa("--source", type=str, default="torchvision",
+    aa("--overall_source", type=str, default="thingsvision")
+    aa("--sources", type=str, nargs="+",
         choices=[
             "custom",
             "timm",
@@ -110,7 +111,7 @@ def create_config_dicts(args) -> Tuple[FrozenDict, FrozenDict]:
     model_cfg.temperatures = get_temperatures(
         model_config, model_cfg.names, args.module
     )
-    model_cfg.source = args.source
+    model_cfg.sources = args.sources
     model_cfg.input_dim = args.input_dim
     model_cfg = config_dict.FrozenConfigDict(model_cfg)
     data_cfg.root = args.data_root
@@ -125,13 +126,19 @@ def evaluate(args) -> None:
     model_cfg, data_cfg = create_config_dicts(args)
     results = []
     model_features = dict()
-    for i, model_name in tqdm(enumerate(model_cfg.names), desc="Model"):
+    for i, (model_name, source) in tqdm(enumerate(zip(model_cfg.names, model_cfg.sources)), desc="Model"):
+        if model_name.startswith('clip'):
+            model_name, variant = model_name.split('_')
+            model_params = {'variant': variant}
+        else:
+            model_params = None
         family_name = utils.analyses.get_family_name(model_name)
         extractor = get_extractor(
             model_name=model_name,
-            source=model_cfg.source,
+            source=source,
             device=device,
             pretrained=not args.not_pretrained,
+            model_parameters=model_params,
         )
         dataset = load_dataset(
             name=args.dataset,
@@ -161,7 +168,7 @@ def evaluate(args) -> None:
             "model": model_name,
             "zero-shot spearman": spearman_rho,
             "zero-shot pearson": pearson_corr_coef,
-            "source": model_cfg.source,
+            "source": source,
             "family": family_name,
             "dataset": data_cfg.name,
         }
@@ -171,7 +178,7 @@ def evaluate(args) -> None:
     # convert results into Pandas DataFrame
     results = pd.DataFrame(results)
 
-    out_path = os.path.join(args.out_path, args.dataset, args.source, args.module)
+    out_path = os.path.join(args.out_path, args.dataset, args.overall_source, args.module)
     if not os.path.exists(out_path):
         print("\nOutput directory does not exist...")
         print("Creating output directory to save results...\n")
