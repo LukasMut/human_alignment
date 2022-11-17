4bfefde8851400b356588484d771daed46ba70f7
LukasMut <muttenthaler@cbs.mpg.de>
2022-11-17 15:10:42 +0100

    updated layer eval

--- main_layer_eval.py
+++ main_layer_eval.py
@@ -4,17 +4,17 @@
 import argparse
 import os
 import random
-from typing import Any, List, Tuple
+from typing import Any
 
 import numpy as np
 import pandas as pd
 import torch
-from torch.utils.data import DataLoader
+from thingsvision import get_extractor
+from thingsvision.utils.data import DataLoader
 from tqdm import tqdm
 
 import utils
 from data import DATASETS, load_dataset
-from models import CustomModel
 
 FrozenDict = Any
 Tensor = torch.Tensor
@@ -32,8 +32,8 @@ def parseargs():
     aa("--model", type=str, help="model for which we want to extract features")
     aa("--layers", type=str, nargs='+', help="module for which to extract features")
     aa("--source", type=str, default="torchvision",
-       choices=["timm", "torchvision", "custom"],
-       help="Host of (pretrained) models")
+       choices=["timm", "torchvision", "custom", "vissl"],
+       help="Source of (pretrained) models")
     aa("--model_dict_path", type=str,
        default="/home/space/datasets/things/model_dict.json",
        help="Path to the model_dict.json")
@@ -55,8 +55,6 @@ def parseargs():
        help="whether to show print statements about model performance during training")
     aa("--not_pretrained", action="store_true",
        help="load random model instead of pretrained")
-    aa("--ssl_models_path", type=str, default="/home/space/datasets/things/ssl-models",
-       help="Path to converted ssl models from vissl library.")
     args = parser.parse_args()
     return args
 
@@ -64,39 +62,37 @@ def parseargs():
 def evaluate(args) -> None:
     device = torch.device(args.device)
     results = []
-    model_features = dict()
+    model_features = {}
     family_name = utils.analyses.get_family_name(args.model)
-    model = CustomModel(
-        model_name=args.model,
-        pretrained=not args.not_pretrained,
-        model_path=None,
-        device=device,
-        source=args.source,
-        ssl_models_path=args.ssl_models_path,
-    )
+    extractor = get_extractor(
+            model_name=args.model,
+            source=args.source,
+            device=device,
+            pretrained=not args.not_pretrained,
+        )
     dataset = load_dataset(
-        name=args.dataset,
-        data_dir=args.data_root,
-        transform=model.get_transformations(),
+            name=args.dataset,
+            data_dir=args.data_root,
+            transform=extractor.get_transformations(),
+        )
+    batches = DataLoader(
+            dataset=dataset,
+            batch_size=args.batch_size,
+            backend=extractor.get_backend(),
     )
-    dl = DataLoader(
-        dataset=dataset, batch_size=args.batch_size, shuffle=False, drop_last=False
-    )
-
     for module_name in tqdm(args.layers, desc="Layer"):
-        features, _ = model.extract_features(
-            data_loader=dl,
+        features = extractor.extract_features(
+            batches=batches,
             module_name=module_name,
             flatten_acts=True,
-            clip=True if args.model.lower().startswith("clip") else False,
-            return_probabilities=False,
         )
         if len(features.shape) >= 3:
             # global average pooling
             features = features.mean(axis=-1).mean(axis=-1)
         triplets = dataset.get_triplets()
         choices, probas = utils.evaluation.get_predictions(
-            features, triplets, 1.0, args.distance
+            features=features, triplets=triplets, 
+            temperature=float(1), dist=args.distance,
         )
         acc = utils.evaluation.accuracy(choices)
         entropies = utils.evaluation.ventropy(probas)
@@ -114,13 +110,13 @@ def evaluate(args) -> None:
             "probas": probas.cpu().numpy(),
             "source": args.source,
             "family": family_name,
+            "dataset": args.dataset,
         }
         results.append(summary)
         model_features[module_name] = features
 
     # convert results into Pandas DataFrame
     results = pd.DataFrame(results)
-    failures = utils.evaluation.get_failures(results)
 
     out_path = args.out_path
     if not os.path.exists(out_path):
@@ -130,7 +126,6 @@ def evaluate(args) -> None:
     # save dataframe to pickle to preserve data types after loading
     # load back with pd.read_pickle(/path/to/file/pkl)
     results.to_pickle(os.path.join(out_path, "results.pkl"))
-    failures.to_pickle(os.path.join(out_path, "failures.pkl"))
     utils.evaluation.save_features(features=model_features, out_path=out_path)
 
 
