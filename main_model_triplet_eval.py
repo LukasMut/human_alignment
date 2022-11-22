4aeef9c654715ca189f32e2811ba9fec91c77590
LukasMut <muttenthaler@cbs.mpg.de>
2022-11-22 16:43:30 +0100

    small fix in model triplet eval

--- main_model_triplet_eval.py
+++ main_model_triplet_eval.py
@@ -31,43 +31,86 @@ def parseargs():
 
     aa("--data_root", type=str, help="path/to/things")
     aa("--dataset", type=str, help="Which dataset to use", choices=DATASETS)
-    aa("--model_names", type=str, nargs="+",
-        help="models for which we want to extract featues")
-    aa("--module", type=str,
+    aa(
+        "--model_names",
+        type=str,
+        nargs="+",
+        help="models for which we want to extract featues",
+    )
+    aa(
+        "--module",
+        type=str,
         choices=["logits", "penultimate"],
-        help="module for which to extract features")
+        help="module for which to extract features",
+    )
     aa("--overall_source", type=str, default="thingsvision")
-    aa("--sources", type=str, nargs="+",
+    aa(
+        "--sources",
+        type=str,
+        nargs="+",
         choices=[
             "custom",
             "timm",
             "torchvision",
             "vissl",
-            ],
+        ],
         help="Source of (pretrained) models",
-        )
-    aa("--model_dict_path", type=str,
+    )
+    aa(
+        "--model_dict_path",
+        type=str,
         default="/home/space/datasets/things/model_dict.json",
-        help="Path to the model_dict.json")
-    aa("--distance", type=str, default="cosine",
+        help="Path to the model_dict.json",
+    )
+    aa(
+        "--distance",
+        type=str,
+        default="cosine",
         choices=["cosine", "euclidean", "dot"],
-        help="distance function used to predict the odd-one-out")
+        help="distance function used to predict the odd-one-out",
+    )
     aa("--input_dim", type=int, default=224, help="input image dimensionality")
-    aa("--batch_size", metavar="B", type=int, default=128,
-        help="number of triplets sampled during each step (i.e., mini-batch size)")
-    aa("--out_path", type=str,
+    aa(
+        "--batch_size",
+        metavar="B",
+        type=int,
+        default=128,
+        help="number of triplets sampled during each step (i.e., mini-batch size)",
+    )
+    aa(
+        "--out_path",
+        type=str,
         default="/home/space/datasets/things/results/",
-        help="path/to/results")
-    aa("--device", type=str, default="cuda",
-        help="whether evaluation should be performed on CPU or GPU (i.e., CUDA).")
-    aa("--num_threads", type=int, default=4,
-        help="number of threads used for intraop parallelism on CPU; use only if device is CPU")
-    aa("--rnd_seed", type=int, default=42,
-        help="random seed for reproducibility of results")
-    aa("--verbose", action="store_true",
-        help="whether to show print statements about model performance during training")
-    aa("--not_pretrained", action="store_true",
-        help="load random model instead of pretrained")
+        help="path/to/results",
+    )
+    aa(
+        "--device",
+        type=str,
+        default="cuda",
+        help="whether evaluation should be performed on CPU or GPU (i.e., CUDA).",
+    )
+    aa(
+        "--num_threads",
+        type=int,
+        default=4,
+        help="number of threads used for intraop parallelism on CPU; use only if device is CPU",
+    )
+    aa(
+        "--rnd_seed",
+        type=int,
+        default=42,
+        help="random seed for reproducibility of results",
+    )
+    aa(
+        "--verbose",
+        action="store_true",
+        help="whether to show print statements about model performance during training",
+    )
+    aa(
+        "--not_pretrained",
+        action="store_true",
+        help="load random model instead of pretrained",
+    )
     args = parser.parse_args()
     return args
 
@@ -113,7 +156,7 @@ def create_config_dicts(args) -> Tuple[FrozenDict, FrozenDict]:
     model_cfg.temperatures = get_temperatures(
         model_config, model_cfg.names, args.module
     )
-    model_cfg.source = args.source
+    model_cfg.sources = args.sources
     model_cfg.input_dim = args.input_dim
     model_cfg = config_dict.FrozenConfigDict(model_cfg)
     data_cfg.root = args.data_root
@@ -128,22 +171,24 @@ def evaluate(args) -> None:
     model_cfg, data_cfg = create_config_dicts(args)
     results = []
     model_features = dict()
-    for i, model_name in tqdm(enumerate(model_cfg.names), desc="Model"):
-        
-        if model_name.startswith('OpenCLIP'):
-            name, variant, data = model_name.split('_')
+    for i, (model_name, source) in tqdm(
+        enumerate(zip(model_cfg.names, model_cfg.sources)), desc="Model"
+    ):
+
+        if model_name.startswith("OpenCLIP"):
+            name, variant, data = model_name.split("_")
             model_params = dict(variant=variant, dataset=data)
-        elif model_name.startswith('clip'):
-            name, variant = model_name.split('_')
+        elif model_name.startswith("clip"):
+            name, variant = model_name.split("_")
             model_params = dict(variant=variant)
         else:
             name = model_name
             model_params = None
-        
+
         family_name = utils.analyses.get_family_name(model_name)
         extractor = get_extractor(
             model_name=name,
-            source=model_cfg.source,
+            source=source,
             device=device,
             pretrained=not args.not_pretrained,
             model_parameters=model_params,
@@ -180,18 +225,20 @@ def evaluate(args) -> None:
             "choices": choices.cpu().numpy(),
             "entropies": entropies.cpu().numpy(),
             "probas": probas.cpu().numpy(),
-            "source": model_cfg.source,
+            "source": source,
             "family": family_name,
             "dataset": data_cfg.name,
         }
         results.append(summary)
-        model_features[model_name] = features
+        model_features[source][model_name][args.module] = features
 
     # convert results into Pandas DataFrame
     results = pd.DataFrame(results)
     failures = utils.evaluation.get_failures(results)
 
-    out_path = os.path.join(args.out_path, args.dataset, args.source, args.module)
+    out_path = os.path.join(
+        args.out_path, args.dataset, args.overall_source, args.module
+    )
     if not os.path.exists(out_path):
         print("\nOutput directory does not exist...")
         print("Creating output directory to save results...\n")
