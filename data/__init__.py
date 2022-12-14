351d9ee1e250dd97899c21cdfe779445b69a063e
LukasMut <muttenthaler@cbs.mpg.de>
2022-12-14 18:33:28 +0100

    updated free arrangement data

--- data/__init__.py
+++ data/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from .cifar import CIFAR10Triplet, CIFAR100CoarseTriplet, CIFAR100Triplet
 from .multi_arrangement import MultiArrangement
+from .free_arrangement import FreeArrangement
 from .peterson import Peterson
 from .things import THINGSBehavior, THINGSTriplet
 
@@ -16,7 +17,7 @@ DATASETS = [
 ]
 
 
-def load_dataset(name: str, data_dir: str, category=None, transform=None):
+def load_dataset(name: str, data_dir: str, category=None, stimulus_set=None, transform=None):
     if name == "cifar100-coarse":
         dataset = CIFAR100CoarseTriplet(
             triplet_path=os.path.join(data_dir, "cifar100_coarse_triplets.npy"),
@@ -35,10 +36,19 @@ def load_dataset(name: str, data_dir: str, category=None, transform=None):
         )
     elif name == "multi-arrangement":
         dataset = MultiArrangement(root=data_dir, transform=transform)
+    elif name == "free-arrangement":
+        assert isinstance(
+            stimulus_set, str
+        ), "\nSimilarity judgments for the data from Groen et al. (2019) were collected for two differen stimulus sets.\nPlease provide the stimulus set.\n"
+        dataset = FreeArrangement(
+            root=data_dir,
+            stimulus_set=stimulus_set,
+            transform=transform,
+        )
     elif name == "peterson":
         assert isinstance(
             category, str
-        ), "\nSimilarity judgments for the data from Peterson et al. (2016) were collecte for individual categories.\nPlease provide a category name.\n"
+        ), "\nSimilarity judgments for the data from Peterson et al. (2016) were collected for individual categories.\nPlease provide a category name.\n"
         dataset = Peterson(
             root=data_dir,
             category=category,
