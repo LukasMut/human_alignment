2b9d5e877908c87194b249e7a3ca662c0a3b9339
LukasMut <muttenthaler@cbs.mpg.de>
2022-12-19 16:32:16 +0100

    fixed path problem

--- data/free_arrangement.py
+++ data/free_arrangement.py
@@ -57,7 +57,7 @@ class FreeArrangement(torch.utils.data.Dataset):
         return pairwise_distances
 
     def __getitem__(self, idx: int) -> Tensor:
-        img = os.path.join(self.root, self.img_subfolder, self.order[idx])
+        img = os.path.join(self.root, self.img_subfolder, self.stimulus_set, self.order[idx])
         img = Image.open(img)
         if self.transform is not None:
             img = self.transform(img)
