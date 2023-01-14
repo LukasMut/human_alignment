27238c5e4b3b363a7798d86049e2ae25dac0a61b
Jonas Dippel <jonas.dippel@gmx.de>
2023-01-14 15:25:50 +0100

    excluded random + seed1 models

--- parse_results.py
+++ parse_results.py
@@ -10,25 +10,38 @@ args = parser.parse_args()
 
 DATASETS = ['multi-arrangement', 'free-arrangement/set1', 'free-arrangement/set2', 'things']
 
+
+def filter_models(df):
+    # SSL models only have penultimate layer
+    ssl_and_logits = (df.source == 'ssl') & (df.module == 'logits')
+    df = df[~ssl_and_logits]
+
+    # Remove inceptionv3 model
+    df = df[df.model != 'inception_v3']
+
+    # We only look at vit-same results
+    df = df[df.source != 'vit_best']
+
+    model_blacklist = []
+    for model in df.model.values:
+        if model.startswith('resnet_v1_50_tpu_random_init') or 'seed1' in model:
+            model_blacklist.append(model)
+    df = df[~df.model.isin(model_blacklist)]
+    return df
+
+
 for dataset in DATASETS:
     if dataset == 'things':
         with open(join(args.results_root, dataset, 'zero-shot', 'all_results.pkl'), 'rb') as f:
             zero_shot_results = pickle.load(f)
         zero_shot_results = zero_shot_results.drop(columns=['choices', 'entropies', 'probas'])
-        # SSL models only have penultimate layer
-        ssl_and_logits = (zero_shot_results.source == 'ssl') & (zero_shot_results.module == 'logits')
-        zero_shot_results = zero_shot_results[~ssl_and_logits]
-        # Remove inceptionv3 model
-        zero_shot_results = zero_shot_results[zero_shot_results.model != 'inception_v3']
         mapper = Mapper(zero_shot_results)
         zero_shot_results['training'] = mapper.get_training_objectives()
 
     else:
         zero_shot_results = parse_results_dir(join(args.results_root, dataset, 'zero-shot'))
 
-    # We only look at vit-same results
-    zero_shot_results = zero_shot_results[zero_shot_results.source != 'vit_best']
-
+    zero_shot_results = filter_models(zero_shot_results)
     # Write results to csv
     zero_shot_results.to_csv(join(args.results_root, dataset, 'zero-shot.csv'))
 
@@ -40,5 +53,6 @@ for dataset in DATASETS:
     else:
         transform_results = parse_results_dir(join(args.results_root, dataset, 'transform'))
 
+    transform_results = filter_models(transform_results)
     # Write results to csv
     transform_results.to_csv(join(args.results_root, dataset, 'transform.csv'))
