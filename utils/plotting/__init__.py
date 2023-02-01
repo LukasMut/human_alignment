175c16cb98000c692c2472202329718ae0d5d376
Jonas Dippel <jonas.dippel@gmx.de>
2023-02-01 19:15:52 +0100

    small plotting changes

--- utils/plotting/__init__.py
+++ utils/plotting/__init__.py
@@ -5,3 +5,4 @@ from .detail import make_detail_plot
 from .transform import zero_shot_vs_transform_plot
 from .clip import clip_plot
 from .logits_penultimate import logits_penultimate_plot
+from .distance_fn import distance_fn_plot
